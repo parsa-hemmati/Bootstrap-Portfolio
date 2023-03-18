fd81c60e3b1bb89857d380db9121533d62bd8670
parsa-hemmati <89730775+parsa-hemmati@users.noreply.github.com>
2023-03-18 22:40:01 +0000

    added descriptions to the projects

--- js/script.js
+++ js/script.js
@@ -3,18 +3,21 @@ const work = [
     "title": "Paris 2024 Olympics Planner",
     "deployed_version": "https://sally077.github.io/Paris-Olympics/",
     "github_repo": "https://github.com/Sally077/Paris-Olympics",
+    "description": "A website that allows users to plan their trip to the 2024 Paris Olympics.",
     "screenshot": "https://raw.githubusercontent.com/parsa-hemmati/Bootstrap-Portfolio/main/images/Paris.jpeg?"
   },
   {
     "title": "Password Generator",
     "deployed_version": "https://parsa-hemmati.github.io/password-generator/",
     "github_repo": "https://github.com/parsa-hemmati/password-generator",
+    "description": "A website that allows users to generate a random password based on criteria they’ve selected.",
     "screenshot": "https://raw.githubusercontent.com/parsa-hemmati/Bootstrap-Portfolio/main/images/Password.jpeg"
   },
   {
     "title": "Javascript Quizz",
     "deployed_version": "https://parsa-hemmati.github.io/JavaScript-Quiz/",
     "github_repo": "https://github.com/parsa-hemmati/JavaScript-Quiz/",
+    "description": "A timed quiz on JavaScript fundamentals that stores high scores.",
     "screenshot": "https://raw.githubusercontent.com/parsa-hemmati/Bootstrap-Portfolio/main/images/Quiz.jpeg"
   }
 ];
@@ -29,7 +32,7 @@ function createCard(work) {
           <img src="${work.screenshot}" class="card-img-top" alt="${work.title}">
           <div class="card-body">
             <h5 class="card-title">${work.title}</h5>
-            <p class="card-text">A brief overview of the project</p>
+            <p class="card-text">${work.description}</p>
             <a href="${work.deployed_version}" class="btn btn-primary">View Project</a>
           </div>
         </div>
